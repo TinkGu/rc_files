f5286158ff31ca329b832043e1f59da9883faa9c
TinkGu <tlwrite@126.com>
2016-10-15 14:42:23 +0800

    Update zsh-update.sh

--- scripts/preset/zsh-update.sh
+++ scripts/preset/zsh-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install zsh_theme
-if [ ! -a "~/.zsh_theme"]; then
+if [-a "~/.zsh_theme"]; then
     theme_sh="source ~/.zsh_theme"
     sed -i "/^source.*oh-my-zsh\.sh/i${theme_sh}" ~/.zshrc
 fi
@@ -9,6 +9,6 @@ curl https://raw.githubusercontent.com/TinkGu/rc_files/master/zsh/.zsh_theme > ~
 
 # add zsh_profile
 curl https://raw.githubusercontent.com/TinkGu/rc_files/master/zsh/.zsh_profile > ~/.zsh_profile
-if [ ! -a "~/.zsh_profile"]; then
+if [-a "~/.zsh_profile"]; then
     echo 'source ~/.zsh_profile' >> ~/.zshrc
 fi
