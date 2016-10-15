c12c0e72446803d9bf241bf92552d6986c44475e
TinkGu <tlwrite@126.com>
2016-10-15 14:36:37 +0800

    add zsh update sh

--- scripts/preset/zsh-install.sh
+++ scripts/preset/zsh-install.sh
@@ -7,15 +7,3 @@ echo 'zsh' >> ~/.bashrc
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# install zsh_theme
-curl https://raw.githubusercontent.com/TinkGu/rc_files/master/zsh/.zsh_theme > ~/.zsh_theme
-theme_sh="source ~/.zsh_theme"
-sed -i "/^source.*oh-my-zsh\.sh/i${theme_sh}" ~/.zshrc
-
-# add zsh_profile
-curl https://raw.githubusercontent.com/TinkGu/rc_files/master/zsh/.zsh_profile > ~/.zsh_profile
-echo 'source ~/.zsh_profile' >> ~/.zshrc
-
-zsh
-echo "welcome zsh"
