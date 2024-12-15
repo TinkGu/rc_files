349ab7dc4f66ac26b45b5b4b3a201be61c7b32f6
TinkGu <tlwrite@126.com>
2024-12-15 15:52:36 +0800

    Update zsh-install.sh

--- scripts/preset/zsh-install.sh
+++ scripts/preset/zsh-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# install zsh
-brew install zsh
-echo 'zsh' >> ~/.bashrc
+# install zsh 【可跳过，因为一般 iterm2 会预装】
+# brew install zsh
+# echo 'zsh' >> ~/.bashrc
 
 # TODO: git clone this repo and init symlink
 
@@ -10,6 +10,6 @@ echo 'zsh' >> ~/.bashrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install zsh_theme
-curl https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme > ~/.oh-my-zsh/themes/bullet-train.zsh-theme
+# curl https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme > ~/.oh-my-zsh/themes/bullet-train.zsh-theme
 
 echo -e '---\ndone!'
